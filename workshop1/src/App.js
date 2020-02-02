2cdbb584cf35cfc9505f98ef587298249ec09a05
sabaijung <ardsri.s@gmail.com>
2020-02-02 16:29:25 +0700

    initial

---
+++ workshop1/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+    return ( <
+        div className = "App" >
+        <
+        h1 > สวั สดี React < /h1>
+        ทดสอบการทำงานของ react js {
+            " "
+        } <
+        /div>
+    );
+}
+
+export default App;
